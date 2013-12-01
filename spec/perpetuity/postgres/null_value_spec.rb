d619954a5ad4819484215d527a49bb790a3078f2
Jamie Gaskins <jgaskins@gmail.com>
2013-12-01 16:35:08 -0500

    Correct spelling error

--- spec/perpetuity/postgres/null_value_spec.rb
+++ spec/perpetuity/postgres/null_value_spec.rb
@@ -3,7 +3,7 @@ require 'perpetuity/postgres/null_value'
 module Perpetuity
   class Postgres
     describe NullValue do
-      it 'serializes into a Postgress NULL value' do
+      it 'serializes into a Postgres NULL value' do
         NullValue.new.to_s.should == 'NULL'
       end
     end
